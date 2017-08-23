be4ad6435d6497c8e69decd06fe7fba59949a095
liangchangchun <changchun.liang@uebuycar.com>
2017-08-23 13:57:21 +0800

    dubbo项目集成

--- lccfang-api/src/main/java/com/lcc/api/RunApplication.java
+++ lccfang-api/src/main/java/com/lcc/api/RunApplication.java
@@ -1,4 +1,4 @@
-package com.lcc.concert;
+package com.lcc.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- lccfang-consume/src/main/java/com/lcc/consume/RunApplication.java
+++ lccfang-consume/src/main/java/com/lcc/consume/RunApplication.java
@@ -1,9 +1,10 @@
-package com.lcc.concert;
+package com.lcc.consume;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 
 @SpringBootApplication
