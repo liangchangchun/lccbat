0d34f85e504cfe4b74ea5dfa343a4fa48793a260
liangchangchun <changchun.liang@uebuycar.com>
2017-08-23 18:55:56 +0800

    更新

--- lccfang-provide/src/main/java/com/lcc/service/config/DubboConfig.java
+++ lccfang-provide/src/main/java/com/lcc/service/config/DubboConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration  
-@PropertySource("classpath:dubbo/dubbo.properties")
-@ImportResource({"classpath:dubbo/dubbo-provider.xml"})  
+//@Configuration  
+//@PropertySource("classpath:dubbo/dubbo.properties")
+//@ImportResource({"classpath:dubbo/dubbo-provider.xml"})  
 public class DubboConfig {
 
 }
