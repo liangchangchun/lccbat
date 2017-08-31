36108ab3b0a38f068a5c796b453c9b5a637bcf67
liangchangchun <changchun.liang@uebuycar.com>
2017-08-31 16:29:31 +0800

    netty通讯服务端与客户端

---
+++ lccfang-netty-client/src/main/java/com/lcc/game/client/RunApplication.java
@@ -0,0 +1,23 @@
+package com.lcc.game.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class RunApplication {
+
+	protected final static Logger logger = LoggerFactory.getLogger(RunApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RunApplication.class,args);
+		 logger.info("RunApplication is sussess!");
+		
+	}
+
+}
