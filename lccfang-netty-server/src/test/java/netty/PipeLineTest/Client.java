045d2d896e30a2576df6016ec75fdbbc4e50aff9
liangchangchun <changchun.liang@uebuycar.com>
2017-08-31 19:39:18 +0800

    netty通讯

---
+++ lccfang-netty-server/src/test/java/netty/PipeLineTest/Client.java
@@ -0,0 +1,17 @@
+package netty.PipeLineTest;
+
+
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) throws Exception {
+
+		Socket socket = new Socket("127.0.0.1", 10101);
+		
+		socket.getOutputStream().write("hello".getBytes());
+		
+		socket.close();
+	}
+
+}
